8c75c3e44432a5299d26da5a0ed88e06d0eb9c2d
YaCpotato <yasshisshy@gmail.com>
2020-12-04 21:23:59 +0900

    your_timeline修正

--- script/your_timeline.py
+++ script/your_timeline.py
@@ -1,17 +1,29 @@
 import json
 import config
 from requests_oauthlib import OAuth1Session
+import csv
 
 def my_timeline():
     url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
-    params ={'screen_name': 'realDonaldTrump','count' : 20}# JoeBiden
+    params ={'screen_name': 'hirasawa','count' : 200}# JoeBiden
+    header = ['id','User Name','User ID','Follows','Followers','User Location','content','time']
     req = twitter.get(url, params = params)
     if req.status_code == 200:
-        timeline = json.loads(req.text)
-        for tweet in timeline:
-            print(tweet['user']['name']+'::'+tweet['text'])
-            print(tweet['created_at'])
-            print('----------------------------------------------------')
+        with open('my_timeline.csv', 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(header)
+            timeline = json.loads(req.text)
+            for tweet in timeline:
+                tmp = []
+                tmp.append(tweet['id'])
+                tmp.append(tweet['user']['name'])
+                tmp.append(tweet['user']['screen_name'])
+                tmp.append(tweet['user']['friends_count'])
+                tmp.append(tweet['user']['followers_count'])
+                tmp.append(tweet['user']['location'])
+                tmp.append(tweet['text'])
+                tmp.append(tweet['created_at'])
+                writer.writerow(tmp)
     else:
         print("ERROR: %d" % req.status_code)
 
